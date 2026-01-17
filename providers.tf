2456436b49f3da8f1f018422dc3ac28ca8350843
balarithwik <rithwik10122000@gmail.com>
2026-01-17 12:31:02 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
-provider "kubernetes" {
-  config_path = var.kubeconfig_path
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+  }
 }
-
-provider "docker" {}
